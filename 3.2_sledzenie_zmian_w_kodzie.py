29df0328d841c5c1307fdf65f160b026dc4a64f8
Piotr Sobala <piotrek0706@gmail.com>
2024-08-14 15:22:23 +0200

    Add 3.2_sledzenie_zmian_w_kodzie.py

--- 3.2_sledzenie_zmian_w_kodzie.py
+++ 3.2_sledzenie_zmian_w_kodzie.py
@@ -9,3 +9,6 @@ zakupy = []
 print("Lista zakupów")
 for sklep,zakupy in sklep_lista.items():
     print(f"Idę do {sklep} i kupuję tam {zakupy}")
+
+produkty = sum(sklep_lista.values(), [])
+print(f"W sumie kupuję {len(produkty)} produktów.")
